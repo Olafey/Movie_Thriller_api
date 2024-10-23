10ea48fe8b3dfdbd4a8a9454813a055bae6c8d90
Olafey <olamidearemu2000@yahoo.com>
2024-10-23 15:09:22 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 const userName = document.getElementById("username");
 const passWord = document.getElementById("password");
 const form = document.getElementById("form");
+
+form.addEventListener("submit", (e)-> {
+    e.target
+});
+
+const Login = async () => {
+  const response = await fetch("");
+  const data = response.json();
+
+  console.log(data);
+};
